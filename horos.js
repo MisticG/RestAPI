4f1fa0698b9ebbf6a4623e8bf9531ef9c06200ca
Divinitatem <bastulampa3@gmail.com>
2019-01-11 21:00:58 +0100

    test

--- horos.js
+++ horos.js
@@ -13,7 +13,7 @@ function inItSite() {
             document.getElementById("tecken").innerHTML = data.name;
             document.getElementById("teckenInfo").innerHTML = data.signInfo;
         }
-        //else gör ingenting
+        //else gör ingentin
     })
     
     }
